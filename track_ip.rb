56a0f9c75e05e8333f0e98912e6f35f1d358d0fa
Diego Cuevas <diegocuevas579@gmail.com>
2021-11-26 18:10:25 -0500

    ADD: solution to track ip

---
+++ track_ip.rb
@@ -0,0 +1,28 @@
+class TrackIp 
+  def initialize
+    @ordered_ips = Hash.new(0)
+  end
+
+  def request_handled(ip)
+    @ordered_ips[ip] += 1
+  end
+
+  def top100
+    @ordered_ips.max_by(100, &:last).map{|ip,points| ip}
+  end
+end
+
+
+require 'faker'
+require 'benchmark'
+
+track_ip = TrackIp.new
+sample_ips = (1..20_000).map { Faker::Internet.public_ip_v4_address }
+n = 20_000_000
+n.times { track_ip.request_handled(sample_ips.sample) }
+p track_ip.top100 
+
+# Benchmark.bm do |x|
+#   x.report("request_handled") { n.times { track_ip.request_handled(sample_ips.sample) } }
+#   x.report("top100")  { track_ip.top100 }
+# end
